c9238f73d0b4f0a19198cc0a2fda312464a74f6e
Bereket <bereketteshome685@gmail.com>
2024-10-11 10:11:59 +0300

    sign in/up bug fixed

--- client/src/components/GlowingBG.tsx
+++ client/src/components/GlowingBG.tsx
@@ -1,11 +1,11 @@
 const GlowingBG = () => {
   return (
-    <div>
+    <div className="hidden lg:block">
       {/* Glowing circle at the top-left */}
-      <div className="absolute top-0 left-0 rounded-full w-72 h-72 bg-gradient-to-r from-blue-400 to-purple-600 blur-3xl opacity-70 animate-pulseGlow"></div>
+      <div className="absolute top-0 left-0 z-0 rounded-full w-72 h-72 bg-gradient-to-r from-blue-400 to-purple-600 blur-3xl opacity-70 animate-pulseGlow"></div>
 
       {/* Glowing circle at the bottom-right */}
-      <div className="absolute bottom-0 right-0 rounded-full w-72 h-72 bg-gradient-to-r from-pink-500 to-yellow-500 blur-3xl opacity-70 animate-pulseGlow"></div>
+      <div className="absolute bottom-0 right-0 z-0 rounded-full w-72 h-72 bg-gradient-to-r from-pink-500 to-yellow-500 blur-3xl opacity-70 animate-pulseGlow"></div>
     </div>
   );
 };
